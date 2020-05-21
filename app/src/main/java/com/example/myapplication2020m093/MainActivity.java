194e2b202887db20f2e339a6c697abf546a9ad29
Asai0918 <65375876+Asai0918@users.noreply.github.com>
2020-05-21 15:41:55 +0900

    homework

--- app/src/main/java/com/example/myapplication2020m093/MainActivity.java
+++ app/src/main/java/com/example/myapplication2020m093/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication2020m093;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogActivity(View view) {
+        Intent toLog = new Intent (this,Main2Activity.class);
+        startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+   Intent toNormal = new Intent (this, NormalActivity.class);
+   startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this,MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void goToHandleActivity(View view) {
+        Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-feve"));
+        startActivity(toLink2);
     }
 }
