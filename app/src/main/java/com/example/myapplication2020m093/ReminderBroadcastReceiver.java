bd4a7160577cb3179f7f3922ec638c361838afd0
Asai0918 <65375876+Asai0918@users.noreply.github.com>
2020-06-12 11:38:32 +0900

    "Homework 5.1: extracted string resources in all layout files"

---
+++ app/src/main/java/com/example/myapplication2020m093/ReminderBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.example.myapplication2020m093;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReminderBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
